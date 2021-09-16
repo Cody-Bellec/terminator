3d939155b077a286f517c1d09f80892655113d94
Cody-Bellec <Cody_Bellec@student.uml.edu>
2021-09-16 13:53:44 -0400

    edit .py

--- packages/hw2/src/hw2.py
+++ packages/hw2/src/hw2.py
@@ -1,26 +1,22 @@
 #!/usr/bin/env python3
-# Adapted from http://wiki.ros.org/ROS/Tutorials/WritingPublisherSubscriber%28python%29
 
 import rospy
 from std_msgs.msg import Float32
 
-class Talker:
+class Homework1:
     def __init__(self):
-        self.pub = rospy.Publisher('input', Float32, queue_size=10)
-    
-    def talk(self):
-        hello_str = rospy.get_time()
-        self.pub.publish(hello_str)
-    
-if __name__ == '__main__':
-    try:
-        t = Talker()
-        rospy.init_node('talker', anonymous=True)
-        rate = rospy.Rate(1) # 1hz
-        while not rospy.is_shutdown():
-            t.talk()
-            rate.sleep()
-    except rospy.ROSInterruptException:
-        pass
+        rospy.Subscriber("output1", Float32, self.callback)
+        self.pub = rospy.Publisher("input", Float32, queue_size=10)
+        self.total = 0
 
+    def callback(self, data):
+        self.total += data.data
+        self.pub.publish(self.total)
+
+if __name__ == '__main__':
+    rospy.init_node('hw2')
+    hw2()
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
 
