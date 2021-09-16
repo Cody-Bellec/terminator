3d8c57554d63c0f319ba801c5684cb4476ac055e
Cody-Bellec <Cody_Bellec@student.uml.edu>
2021-09-16 15:33:24 -0400

    subscriber

---
+++ packages/homework2/src/homework2_1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# adapted from http://wiki.ros.org/ROS/Tutorials/WritingPublisherSubscriber%28python%29
+
+import rospy
+from std_msgs.msg import Float32
+
+class Listener:
+    def __init__(self):
+        rospy.Subscriber("output1", Float32, self.callback)
+
+    def callback(self, data):
+        rospy.loginfo(data.data)
+    
+
+if __name__ == '__main__':
+    rospy.init_node('listener', anonymous=True)
+    Listener()
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+
