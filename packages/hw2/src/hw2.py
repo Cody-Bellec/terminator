5d5b1707743730daac0c9fac70e3011c5a45f8a3
Cody-Bellec <Cody_Bellec@student.uml.edu>
2021-09-16 13:22:38 -0400

    hw2 src

---
+++ packages/hw2/src/hw2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# Adapted from http://wiki.ros.org/ROS/Tutorials/WritingPublisherSubscriber%28python%29
+
+import rospy
+from std_msgs.msg import Float32
+
+class Talker:
+    def __init__(self):
+        self.pub = rospy.Publisher('/homework1/delta', Float32, queue_size=10)
+    
+    def talk(self):
+        hello_str = rospy.get_time()
+        self.pub.publish(hello_str)
+    
+if __name__ == '__main__':
+    try:
+        t = Talker()
+        rospy.init_node('talker', anonymous=True)
+        rate = rospy.Rate(1) # 1hz
+        while not rospy.is_shutdown():
+            t.talk()
+            rate.sleep()
+    except rospy.ROSInterruptException:
+        pass
+
+
