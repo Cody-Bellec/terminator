a35b5c744e718e97cd26857e6a090ce394e9fa8f
Cody-Bellec <Cody_Bellec@student.uml.edu>
2021-11-30 09:42:09 -0800

    hw9

---
+++ packages/homework9/PIDlaunchhw9.py
@@ -0,0 +1,14 @@
+launch>
+
+<param name="controller_ready" type="string" value="true" />
+
+  <group ns="homework5">
+    <param name="controller_ready" type="string" value="true" />
+    <node pkg="homework5" name="PIDnodehw5" type="PIDnodehw5.py"/>
+  </group>
+  
+  <group ns="controls_hw">
+    <param name="controller_ready" type="string" value="true" />
+    <node pkg="controls_hw" name="vehicle_dynamics" type="vehicle_dynamics.py"/>
+  </group>
+</launch>
