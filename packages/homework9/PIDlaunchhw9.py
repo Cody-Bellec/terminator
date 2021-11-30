803c17e4036e1f78f82e0451ec2217954553d00a
Cody-Bellec <Cody_Bellec@student.uml.edu>
2021-11-30 10:41:01 -0800

    hw9

--- packages/homework9/PIDlaunchhw9.py
+++ packages/homework9/PIDlaunchhw9.py
@@ -2,9 +2,9 @@
 
 <param name="controller_ready" type="string" value="true" />
 
-  <group ns="homework5">
+  <group ns="homework9">
     <param name="controller_ready" type="string" value="true" />
-    <node pkg="homework5" name="PIDnodehw5" type="PIDnodehw5.py"/>
+    <node pkg="homework9" name="PIDnodehw9" type="PIDnodehw9.py"/>
   </group>
   
   <group ns="controls_hw">
