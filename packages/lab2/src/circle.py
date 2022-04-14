40edcf2bbc390b245054ed2f8d3c6130b32e3149
Cody-Bellec <Cody_Bellec@student.uml.edu>
2022-04-14 17:58:31 -0400

    Lab2

--- packages/lab2/src/circle.py
+++ packages/lab2/src/circle.py
@@ -3,10 +3,13 @@
 import rospy
 from std_msgs.msg import Float32
 from duckietown_msgs.msg import Twist2DStamped
+from duckietown_msgs.msg import FSMState
 
 class Circle:
     def __init__(self):
         self.pub = rospy.Publisher("/doczy/wheels_driver_node/wheels_cmd", Twist2DStamped, queue_size=10)
+        self.sub = rospy.Subscriber("/doczy/fsm_node/mode", FSMState, self.callback)
+        self.flag = True
 
     def moving(self, speed, turn_ratio):
         
@@ -16,13 +19,19 @@ class Circle:
           
         self.pub.publish(self.turnout)
 
+     def callback(self, message):
+    	if message.state == "LANE_FOLLOWING" and self.flag == True:
+    		
+    		self.flag = False
+    		
+    		c.moving(0.4, 3.8)
+        	rospy.sleep(10)
+        	c.moving(0, 0)
 if __name__ == '__main__':
     try:
+    	rospy.init_node('Circle', anonymous=True)
         c = Circle()
-        rospy.init_node('Circle', anonymous=True)
-        c.moving(0.4, 3.8)
-        rospy.sleep(10)
-        c.moving(0, 0)
+        rospy.spin()
         
     except rospy.ROSInterruptException:
         pass
--- packages/lab2/src/straight.py
+++ packages/lab2/src/straight.py
@@ -3,10 +3,13 @@
 import rospy
 from std_msgs.msg import Float32
 from duckietown_msgs.msg import Twist2DStamped
+from duckietown_msgs.msg import FSMState
 
 class Straight:
     def __init__(self):
         self.pub = rospy.Publisher("/doczy/wheels_driver_node/wheels_cmd", Twist2DStamped, queue_size=10)
+        self.sub = rospy.Subscriber("/doczy/fsm_node/mode", FSMState, self.callback)
+        self.flag = True
 
     def moving(self, speed):
         
@@ -14,16 +17,22 @@ class Straight:
         self.turnout.v = speed
           
         self.pub.publish(self.turnout)
+    
+    def callback(self, message):
+    	if message.state == "LANE_FOLLOWING" and self.flag == True:
+    		
+    		self.flag = False
+    		
+    		c.moving(0.4, 0.6)
+        	rospy.sleep(2.5)
+        	c.moving(0.0, 0.0) #stop
+        	c.moving(0, 0)     #completely stops after all turns are made
 
 if __name__ == '__main__':
     try:
-        c = Straight()
         rospy.init_node('Straight Line', anonymous=True)
-        
-        c.moving(0.4, 0.6)
-        rospy.sleep(2.5)
-        c.moving(0.0, 0.0) #stop
-        c.moving(0, 0)     #completely stops after all turns are made
+        c = Straight()
+        rospy.spin()
         
     except rospy.ROSInterruptException:
         pass
